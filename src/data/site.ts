7e7c90eb2d486681216c886c9b7fcf08c17a5319
sowisgone <sowisgone@gmail.com>
2026-05-19 22:56:11 -0300

    v1.1.2

--- src/data/site.ts
+++ src/data/site.ts
@@ -7,12 +7,13 @@ export const socials = [
 
 export const projects = ['sweety inc, a pc tweaker', 'skulldye, a game'];
 
-export const skills = ['js', 'writting', 'mcfunction', 'bat'];
+export const skills = ['js', 'writting', 'mcfunction', 'bat', 'blender', 'godot'];
 
 export const about = {
 	age: '18y',
-	location: 'Brazil, PR',
+	location: 'Brazil',
 	email: 'sowisgone@gmail.com',
+	description: 'Been making small projects since 2019, participating in many projects. I\'ve worked as a scriptwritter and RPG master for a decade now. Currently, my main focus is working on my future game, SKULLDYE, and doing some stuff in Minecraft. Currently learning Blender, FLstudio and Godot. Feel welcome to email me or send me a message on Discord asking anything. In case you wanna collab with me on something, contact me over Discord for a quicker response!'
 };
 
 export const posts = [{ label: 'blog', href: '/blog' }];
