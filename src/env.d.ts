04d65a3976f809d3981ebfe450cac39f6c57790f
Giordano Menegazzo <sowisgone@gmail.com>
2026-07-30 12:34:18 -0300

    changes to .env behavior

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,8 +2,10 @@
 
 interface ImportMetaEnv {
 	readonly DISCORD_USER_ID: string;
+	readonly GITHUB_TOKEN: string;
 	readonly STEAM_API_KEY: string;
 	readonly STEAM_ID: string;
+	readonly PUBLIC_GITHUB_TOKEN: string;
 }
 
 interface ImportMeta {
